9130604e9c90eb150e6ffeb05306656cac622ebe
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-03-24 10:07:06 +0530

    Update SignalProc.py

--- VITSunday/SignalProc.py
+++ VITSunday/SignalProc.py
@@ -10,12 +10,10 @@ with open("1.txt") as f:
     for line in f:
         for word in line.split(','):
             hue.append(word)
- '''           
 #print(hue)
 #print(len(hue))
 from matplotlib import pyplot as plt
 import math
-
 N = len(rr.green)
 K = N
 if N % 4 ==1:
@@ -32,71 +30,52 @@ rr.green.extend(zeroes)
 rr.blue.extend(zeroes)
 rr.saturation.extend(zeroes)
 rr.value.append(zeroes)
-
-
 y = fft(rr.hue)
 y1 = fft(rr.red)
 y2 = fft(rr.green)
 y3 = fft(rr.blue)
 y4 = fft(rr.saturation)
 #y5 = fft(value)
-
-
 dhue = signal.detrend(rr.hue)
 dred= signal.detrend(rr.red)
 dgreen = signal.detrend(rr.green)
 dblue = signal.detrend(rr.blue)
 dsaturation = signal.detrend(rr.saturation)
 #dvalue = signal.detrend(value)
-
-
-
 yd = fft(dhue)
 yd1 = fft(dred)
 yd2 = fft(dgreen)
 yd3 = fft(dblue)
 yd4 = fft(dsaturation)
 #yd5 = fft(dvalue)
-
-
-
 #N = 352
 # sample spacing
 T = 1.0 / 100.0
 x = np.linspace(0.0, N*T, N)
-
 y = np.array(rr.hue)
 y1 = np.array(rr.red)
 y2 = np.array(rr.green)
 y3 = np.array(rr.blue)
 y4 = np.array(rr.saturation)
 #y5 = np.array(value)
-
-
 yf = fft(y)
 yf1 = fft(y1)
 yf2 = fft(y2)
 yf3 = fft(y3)
 yf4 = fft(y4)
 #yf5 = fft(y5)
-
-
 realyf = yf.real
 realyf1 = yf1.real
 realyf2 = yf2.real
 realyf3 = yf3.real
 realyf4 = yf4.real
 #realyf5 = yf5.real
-
-
 yg = np.abs(yd[:N//2])
 yg1 = np.abs(yd1[:N//2])
 yg2 = np.abs(yd2[:N//2])
 yg3 = np.abs(yd3[:N//2])
 yg4 = np.abs(yd4[:N//2])
 #yg5 = np.abs(yd5[:N//2])
-
-
 #yg = yg[2:]
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 #xf = xf[2:]
@@ -212,21 +191,15 @@ for ij in range(len(b0)):
                 max0 = b0[ij]
                 maxheart0 = a0[ij]
                                         
-
-
-
-
 fig, ax = plt.subplots()
 #ax.plot(xf*10-15, yg)
 #ax.plot(xf, 2.0/N * np.abs(yd[:N//2]))
 #plt.show()
-
 #ax.plot(xf*10-15, yg1)
 #ax.plot(xf*10-15, yg2)
 ax.plot(xf*10, yg3)
 #ax.plot(xf*10, yg4)
 #ax.plot(xf*10-5, yg5)
-
 plt.show()
 #maxheart3=89.5
 #import math
@@ -237,7 +210,6 @@ avg = (round((maxheart0 + maxheart1 + maxheart2 + maxheart3 + maxheart4)/5)-10)
 #time = firebase.put('/Patients/Lohith','Time',rr.gg)
 Tyme = str(rr.ct)   
 Tyme2 = Tyme.replace(":", "_")
-
 #print(Tyme3)
 result = firebase.put('/Patients/Lohith/Time_Stamp',Tyme2,avg)
 #import plotly
@@ -278,27 +250,19 @@ result = firebase.put('/Patients/Lohith/Time_Stamp',Tyme2,avg)
 #data = [xf1, yg1]
 #py.iplot(data, filename='Vybhav')
 #
-
 #plt.figure(figsize=(5, 4))
 #plt.plot( hue, label="x")
 #plt.plot( dhue, label="x_detrended")
 #plt.legend(loc='best')
 #plt.show()
-
 #plt.plot(xf, 2.0/N * np.abs(realyf[0:N/2]))
 #plt.grid()
 #plt.show()
-
-
-
 #plt.figure(figsize=(5, 4))
 #plt.plot( hue, label="x")
 #plt.plot( dhue, label="x_detrended")
 #plt.legend(loc='best')
 #plt.show()
-
 #plt.plot(xf, 2.0/N * np.abs(realyf[0:N/2]))
 #plt.grid()
 #plt.show()
-
-
